245ee10111deba6f7077b41e3386c289d8eedb2b
jr <58833849+foreverwoods@users.noreply.github.com>
2024-04-19 05:53:20 -0400

    models and results completion

--- scripts/03-model.R
+++ scripts/03-model.R
@@ -18,9 +18,29 @@ nba_data <- read_parquet("./data/analysis_data/analysis_data.parquet")
 
 ### Model data ####
 
+threes_model <- 
+  stan_glm(
+    formula = PTS ~ `3PA` + `3P%`,
+    data = nba_data,
+    family = gaussian(),
+    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_aux = exponential(rate = 1, autoscale = TRUE),
+    seed = 111
+  )
+
+
+#### Save model ####
+
+saveRDS(
+  threes_model,
+  file = "models/threes_model.rds"
+)
+
+
 scoring_model <- 
   stan_glm(
-    formula = PTS ~  
+    formula = PTS ~ `3PA` + `3P%` + `FGA` + `FG%` + `FTA` + `FT%`,
     data = nba_data,
     family = gaussian(),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
@@ -36,3 +56,44 @@ saveRDS(
   scoring_model,
   file = "models/scoring_model.rds"
 )
+
+
+intangibles_model <- 
+  stan_glm(
+    formula = PTS ~ `ORB` + `AST` + `Pace` + `TOV`,
+    data = nba_data,
+    family = gaussian(),
+    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_aux = exponential(rate = 1, autoscale = TRUE),
+    seed = 111
+  )
+
+
+#### Save model ####
+
+saveRDS(
+  intangibles_model,
+  file = "models/intangibles_model.rds"
+)
+
+
+defense_model <- 
+  stan_glm(
+    formula = PTS ~  STL + BLK + PF + DRB,
+    data = nba_data,
+    family = gaussian(),
+    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_aux = exponential(rate = 1, autoscale = TRUE),
+    seed = 111
+  )
+
+
+#### Save model ####
+
+saveRDS(
+  defense_model,
+  file = "models/defense_model.rds"
+)
+
